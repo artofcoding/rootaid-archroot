0db0b973f87f8ad3269ea87b890a39a0ccc49680
Ralf Bensmann <ralf@art-of-coding.eu>
2020-05-17 20:33:48 +0200

    Docker

--- archroot-docker.sh
+++ archroot-docker.sh
@@ -25,21 +25,43 @@ case "${mode}" in
         pacman --noconfirm -S docker
         systemctl enable docker
         systemctl restart docker
+        pacman --noconfirm -S docker-compose
         pacman --noconfirm -Scc
+        cat >/etc/logrotate.d/docker <<EOF
+/var/lib/docker/containers/*/*.log {
+        rotate 30
+        daily
+        compress
+        missingok
+        delaycompress
+        copytruncate
+}
+EOF
+    ;;
+    install-php)
+        pushd php >/dev/null
+        for dir in php-*
+        do
+            pushd ${dir} >/dev/null \
+                && ./run.sh \
+                && popd >/dev/null
+        done
+        popd >/dev/null
     ;;
     install-portainer)
         docker volume create portainerdata
         docker run \
             -d \
-            -p 127.0.0.1:8000:8000 \
-            -p 127.0.0.1:9000:9000 \
-            --name=portainer \
-            --restart=unless-stopped \
+            --restart unless-stopped \
+            --name portainer \
             -v /var/run/docker.sock:/var/run/docker.sock \
             -v portainerdata:/data \
+            -p 127.0.0.1:8000:8000 \
+            -p 127.0.0.1:9000:9000 \
             portainer/portainer
     ;;
     portainer-cert)
+        hostname="$(hostname -f)"
         certbot certonly \
             --register-unsafely-without-email --agree-tos --no-eff-email \
             --webroot --webroot-path=/var/lib/letsencrypt \
@@ -47,33 +69,12 @@ case "${mode}" in
             --hsts \
             --staple-ocsp --must-staple \
             -n \
-            -d portainer.$(hostname -f)
-        chmod 644 /etc/letsencrypt/archive/portainer.$(hostname -f)/privkey*.pem
-        cat >/srv/http/conf/nginx.d/enabled/portainer.$(hostname -f).conf <<EOF
-server {
-    listen 443;
-    listen [::]:443;
-    server_name portainer.$(hostname -f);
-    access_log /srv/http/logs/nginx/portainer.$(hostname -f)-tls-access.log main;
-    access_log /srv/http/logs/nginx/portainer.$(hostname -f)-tls-scripts.log scripts;
-    error_log /srv/http/logs/nginx/portainer.$(hostname -f)-tls-error.log;
-    ssl_certificate /etc/letsencrypt/live/portainer.$(hostname -f)/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/portainer.$(hostname -f)/privkey.pem;
-    ssl_trusted_certificate /etc/letsencrypt/live/portainer.$(hostname -f)/fullchain.pem;
-    ssl_stapling on;
-    ssl_stapling_verify on;
-    charset utf-8;
-    include /srv/http/conf/nginx.d/inc/gzip-std.conf;
-    include /srv/http/conf/nginx.d/inc/ssl-std.conf;
-    include /srv/http/conf/nginx.d/inc/security-std.conf;
-    include /srv/http/conf/nginx.d/inc/location-std.conf;
-    root /srv/http/sites/deadend/;
-    location / {
-        proxy_pass http://127.0.0.1:9000/;
-    }
-}
-EOF
-        if [[ ! -d /etc/letsencrypt/archive/portainer.$(hostname -f) ]]
+            -d portainer.${hostname}
+        chmod 644 /etc/letsencrypt/archive/portainer.${hostname}/privkey*.pem
+        cat nginx/disabled/portainer.tmpl.conf \
+            | sed -e "s#HOSTNAME#${hostname}#g" \
+            >/srv/http/conf/nginx.d/enabled/portainer.${hostname}.conf
+        if [[ ! -d /etc/letsencrypt/archive/portainer.${hostname} ]]
         then
             certbot certonly \
                 --register-unsafely-without-email --agree-tos --no-eff-email \
@@ -82,14 +83,14 @@ EOF
                 --hsts \
                 --staple-ocsp --must-staple \
                 -n \
-                -d portainer.$(hostname -f)
+                -d portainer.${hostname}
             chmod 755 /etc/letsencrypt/archive
-            chmod 755 /etc/letsencrypt/archive/portainer.$(hostname -f)
-            chmod 644 /etc/letsencrypt/archive/portainer.$(hostname -f)/privkey*.pem
+            chmod 755 /etc/letsencrypt/archive/portainer.${hostname}
+            chmod 644 /etc/letsencrypt/archive/portainer.${hostname}/privkey*.pem
             chmod 755 /etc/letsencrypt/live
         fi
         gixy
-        systemctl restart nginx
+        nginx -s reload
     ;;
 esac
 
