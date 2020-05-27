ef6ce1fb4f5af75371e4ef25df6fadeac6552b51
Ralf Bensmann <ralf@art-of-coding.eu>
2020-05-27 09:18:59 +0200

    Fixed

--- storage/storage.sh
+++ storage/storage.sh
@@ -18,7 +18,7 @@ usage() {
 mode=$1 ; shift
 case "${mode}" in
     destroy)
-        [[ $# -eq 1 ]] && all=$1
+        [[ $# -eq 1 ]] && all=$1 || all="no"
         pushd "${execdir}" >/dev/null
         export $(cat "${execdir}"/variables.env)
         docker-compose down
