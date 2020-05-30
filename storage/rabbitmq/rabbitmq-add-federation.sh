e139cc47ce31d8b452185908e511daca3ab6e735
Ralf Bensmann <ralf@art-of-coding.eu>
2020-05-30 17:04:49 +0200

    RabbitMQ federation

--- storage/rabbitmq/rabbitmq-add-federation.sh
+++ storage/rabbitmq/rabbitmq-add-federation.sh
@@ -4,18 +4,23 @@
 set -o nounset
 set -o errexit
 
-node_uris=("amqp://rabbitmq1:5672" "amqp://rabbitmq2:5673" "amqp://rabbitmq3:5674")
-my_name="$(hostname -s)"
+domain="$(hostname -d)"
+domain="${domain##shard?.}"
+#"rabbitmq.shard3.${domain}"
+node_uris=("rabbitmq.shard1.${domain}" "rabbitmq.shard2.${domain}")
+my_name="$(hostname -f)"
 
-for n in ${#node_uris[@]}
+last_idx=$((${#node_uris[@]} - 1))
+for n in $(seq 0 ${last_idx})
 do
     other_node="${node_uris[$n]}"
     if [[ ${my_name} != ${other_node} ]]
     then
-        echo "${my_name}: Adding federation upstream ${other_node}"
-        rabbitmqctl set_parameter federation-upstream "rabbitmq-shard${n}" '{"uri":"${other_node}"}'
+        rabbitmqctl set_parameter federation-upstream "rabbitmq-shard$((${n} + 1))" "{\"uri\":\"amqps://${other_node}:5671\"}"
     fi
 done
-rabbitmqctl set_policy --apply-to exchanges federate-me "^amq\." '{"federation-upstream-set":"all"}'
+#rabbitmqctl set_policy --priority 10 --apply-to exchanges federate-amq "^amq\." '{"federation-upstream-set":"all"}'
+rabbitmqctl set_policy --priority 10 --apply-to exchanges federate-hbd-exchanges "^hbd\.fed\." '{"federation-upstream-set":"all"}'
+#rabbitmqctl set_policy --priority 10 --apply-to queues federate-hbd-queue "^hbd\." '{"federation-upstream-set":"all"}'
 
 exit 0
--- storage/rabbitmq/rabbitmq-firstrun.sh
+++ storage/rabbitmq/rabbitmq-firstrun.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 # Copyright (C) 2020 art of coding UG, Hamburg
 
+VHOST="hdb"
+
 set -o nounset
 set -o errexit
 
 echo "Waiting for RabbitMQ to come up"
 sleep 10
 
+rabbitmqctl add_vhost "${HBD}"
+
 rabbitmqctl add_user federator federator
 rabbitmqctl set_user_tags federator administrator
-rabbitmqctl set_permissions -p / federator ".*" ".*" ".*"
+rabbitmqctl set_permissions -p "${HBD}" federator ".*" ".*" ".*"
 
 rabbitmqctl add_user bugs bunny
-rabbitmqctl set_permissions -p / bugs ".*" ".*" ".*"
+rabbitmqctl set_permissions -p "${HBD}" bugs ".*" ".*" ".*"
 
 rabbitmqctl list_users
 
